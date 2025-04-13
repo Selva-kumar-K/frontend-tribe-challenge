1edfc188371e3bb64aa3525e1d6a82cb9c6ad92b
Selva-kumar-K <selvanithish2014@gmail.com>
2025-04-13 08:18:23 +0530

    added all functionalities for todoList

--- react/src/components/TaskCard.jsx
+++ react/src/components/TaskCard.jsx
@@ -1,209 +1,37 @@
 import { twMerge } from "tailwind-merge";
 import { Star, Trash2 } from "lucide-react";
-import { useState } from "react";
 
-export const todosData = [
-  {
-    id: 1,
-    text: "Do something nice for someone you care about",
-    completed: false,
-    important: true,
-  },
-  {
-    id: 2,
-    text: "Memorize a poem",
-    completed: true,
-    important: false,
-  },
-  {
-    id: 3,
-    text: "Watch a classic movie",
-    completed: true,
-    important: false,
-  },
-  {
-    id: 4,
-    text: "Watch a documentary",
-    completed: false,
-    important: true,
-  },
-  {
-    id: 5,
-    text: "Invest in cryptocurrency",
-    completed: false,
-    important: true,
-  },
-  {
-    id: 6,
-    text: "Contribute code or a monetary donation to an open-source software project",
-    completed: false,
-    important: true,
-  },
-  {
-    id: 7,
-    text: "Solve a Rubik's cube",
-    completed: true,
-    important: false,
-  },
-  {
-    id: 8,
-    text: "Bake pastries for yourself and neighbor",
-    completed: true,
-    important: false,
-  },
-  {
-    id: 9,
-    text: "Go see a Broadway production",
-    completed: false,
-    important: true,
-  },
-  {
-    id: 10,
-    text: "Write a thank you letter to an influential person in your life",
-    completed: true,
-    important: true,
-  },
-  {
-    id: 11,
-    text: "Invite some friends over for a game night",
-    completed: false,
-    important: false,
-  },
-  {
-    id: 12,
-    text: "Have a football scrimmage with some friends",
-    completed: false,
-    important: false,
-  },
-  {
-    id: 13,
-    text: "Text a friend you haven't talked to in a long time",
-    completed: true,
-    important: true,
-  },
-  {
-    id: 14,
-    text: "Organize pantry",
-    completed: false,
-    important: false,
-  },
-  {
-    id: 15,
-    text: "Buy a new house decoration",
-    completed: true,
-    important: false,
-  },
-  {
-    id: 16,
-    text: "Plan a vacation you've always wanted to take",
-    completed: true,
-    important: true,
-  },
-  {
-    id: 17,
-    text: "Clean out car",
-    completed: false,
-    important: false,
-  },
-  {
-    id: 18,
-    text: "Draw and color a Mandala",
-    completed: true,
-    important: false,
-  },
-  {
-    id: 19,
-    text: "Create a cookbook with favorite recipes",
-    completed: true,
-    important: true,
-  },
-  {
-    id: 20,
-    text: "Bake a pie with some friends",
-    completed: false,
-    important: false,
-  },
-  {
-    id: 21,
-    text: "Create a compost pile",
-    completed: false,
-    important: false,
-  },
-  {
-    id: 22,
-    text: "Take a hike at a local park",
-    completed: true,
-    important: true,
-  },
-  {
-    id: 23,
-    text: "Take a class at local community center that interests you",
-    completed: true,
-    important: true,
-  },
-  {
-    id: 24,
-    text: "Research a topic interested in",
-    completed: true,
-    important: true,
-  },
-  {
-    id: 25,
-    text: "Plan a trip to another country",
-    completed: false,
-    important: true,
-  },
-  {
-    id: 26,
-    text: "Improve touch typing",
-    completed: false,
-    important: false,
-  },
-  {
-    id: 27,
-    text: "Learn React",
-    completed: false,
-    important: true,
-  },
-  {
-    id: 28,
-    text: "Learn calligraphy",
-    completed: false,
-    important: false,
-  },
-  {
-    id: 29,
-    text: "Have a photo session with some friends",
-    completed: true,
-    important: false,
-  },
-  {
-    id: 30,
-    text: "Go to the gym",
-    completed: true,
-    important: true,
-  },
-];
-
-export default function TaskCard({ todosData, title }) {
-  const [todos, setTodos] = useState(todosData);
+export default function TaskCard({ todosData, title, setTodos }) {
   return (
     <div className="mt-10">
       <h1 className="text-lg text-zinc-900 dark:text-zinc-100">{title}</h1>
       <div className="mt-4">
         {/* Task */}
-        {todos.map((todo) => {
+        {todosData.length === 0 && <p className="text-zinc-500">No tasks</p>}
+        {todosData.map((todo) => {
           return (
             <div
               key={todo.id}
-              className="py-4 border-b border-zinc-300 dark:border-zinc-700"
+              className={twMerge(
+                "py-4 border-b border-zinc-300 dark:border-zinc-700",
+                todo.completed && "opacity-50"
+              )}
             >
               <div className="flex justify-between items-center">
                 <label className="flex gap-3">
                   <input
                     type="checkbox"
-                    checked={todo.completed}
                     className=""
+                    checked={todo.completed}
+                    onChange={() => {
+                      setTodos?.((prev) => {
+                        return prev.map((todos) =>
+                          todos.id === todo.id
+                            ? { ...todos, completed: !todos.completed }
+                            : todos
+                        );
+                      });
+                    }}
                   />
                   <span
                     className={twMerge(
@@ -216,13 +44,30 @@ export default function TaskCard({ todosData, title }) {
                 </label>
                 <div className="flex gap-3">
                   <Star
+                    onClick={() =>
+                      setTodos?.((prev) => {
+                        return prev.map((todos) =>
+                          todos.id === todo.id
+                            ? { ...todos, important: !todos.important }
+                            : todos
+                        );
+                      })
+                    }
                     size={20}
                     className={twMerge(
-                      "stroke-zinc-400",
+                      "stroke-zinc-400 cursor-pointer",
                       todo.important && "stroke-pink-500 fill-pink-500"
                     )}
                   />
-                  <Trash2 size={20} className="stroke-zinc-400" />
+                  <Trash2
+                    size={20}
+                    className="stroke-zinc-400 cursor-pointer hover:stroke-zinc-500"
+                    onClick={() =>
+                      setTodos?.((prev) => {
+                        return prev.filter((todos) => todos.id !== todo.id);
+                      })
+                    }
+                  />
                 </div>
               </div>
             </div>
--- react/src/components/TaskList.jsx
+++ react/src/components/TaskList.jsx
@@ -1,18 +1,226 @@
 import { Search, Sun, Moon } from "lucide-react";
 
-import TaskCard, { todosData } from "./TaskCard";
+import TaskCard from "./TaskCard";
 import { useState } from "react";
 import { twMerge } from "tailwind-merge";
 
+export const todosData = [
+  {
+    id: 1,
+    text: "Do something nice for someone you care about",
+    completed: false,
+    important: true,
+  },
+  {
+    id: 2,
+    text: "Memorize a poem",
+    completed: true,
+    important: false,
+  },
+  {
+    id: 3,
+    text: "Watch a classic movie",
+    completed: true,
+    important: false,
+  },
+  {
+    id: 4,
+    text: "Watch a documentary",
+    completed: false,
+    important: true,
+  },
+  {
+    id: 5,
+    text: "Invest in cryptocurrency",
+    completed: false,
+    important: true,
+  },
+  {
+    id: 6,
+    text: "Contribute code or a monetary donation to an open-source software project",
+    completed: false,
+    important: true,
+  },
+  {
+    id: 7,
+    text: "Solve a Rubik's cube",
+    completed: true,
+    important: false,
+  },
+  {
+    id: 8,
+    text: "Bake pastries for yourself and neighbor",
+    completed: true,
+    important: false,
+  },
+  {
+    id: 9,
+    text: "Go see a Broadway production",
+    completed: false,
+    important: true,
+  },
+  {
+    id: 10,
+    text: "Write a thank you letter to an influential person in your life",
+    completed: true,
+    important: true,
+  },
+  {
+    id: 11,
+    text: "Invite some friends over for a game night",
+    completed: false,
+    important: false,
+  },
+  {
+    id: 12,
+    text: "Have a football scrimmage with some friends",
+    completed: false,
+    important: false,
+  },
+  {
+    id: 13,
+    text: "Text a friend you haven't talked to in a long time",
+    completed: true,
+    important: true,
+  },
+  {
+    id: 14,
+    text: "Organize pantry",
+    completed: false,
+    important: false,
+  },
+  {
+    id: 15,
+    text: "Buy a new house decoration",
+    completed: true,
+    important: false,
+  },
+  {
+    id: 16,
+    text: "Plan a vacation you've always wanted to take",
+    completed: true,
+    important: true,
+  },
+  {
+    id: 17,
+    text: "Clean out car",
+    completed: false,
+    important: false,
+  },
+  {
+    id: 18,
+    text: "Draw and color a Mandala",
+    completed: true,
+    important: false,
+  },
+  {
+    id: 19,
+    text: "Create a cookbook with favorite recipes",
+    completed: true,
+    important: true,
+  },
+  {
+    id: 20,
+    text: "Bake a pie with some friends",
+    completed: false,
+    important: false,
+  },
+  {
+    id: 21,
+    text: "Create a compost pile",
+    completed: false,
+    important: false,
+  },
+  {
+    id: 22,
+    text: "Take a hike at a local park",
+    completed: true,
+    important: true,
+  },
+  {
+    id: 23,
+    text: "Take a class at local community center that interests you",
+    completed: true,
+    important: true,
+  },
+  {
+    id: 24,
+    text: "Research a topic interested in",
+    completed: true,
+    important: true,
+  },
+  {
+    id: 25,
+    text: "Plan a trip to another country",
+    completed: false,
+    important: true,
+  },
+  {
+    id: 26,
+    text: "Improve touch typing",
+    completed: false,
+    important: false,
+  },
+  {
+    id: 27,
+    text: "Learn React",
+    completed: false,
+    important: true,
+  },
+  {
+    id: 28,
+    text: "Learn calligraphy",
+    completed: false,
+    important: false,
+  },
+  {
+    id: 29,
+    text: "Have a photo session with some friends",
+    completed: true,
+    important: false,
+  },
+  {
+    id: 30,
+    text: "Go to the gym",
+    completed: true,
+    important: true,
+  },
+];
 export default function TaskList() {
   const [darkMode, setDarkMode] = useState(false);
-  const importantTodos = todosData.filter(
+  const [todos, setTodos] = useState(todosData);
+  const [newTask, setNewTask] = useState("");
+
+  const importantTodos = todos.filter(
     (todo) => todo.important && !todo.completed
   );
-  const notImportantTodos = todosData.filter(
+  const notImportantTodos = todos.filter(
     (todo) => !todo.important && !todo.completed
   );
-  const completedTodos = todosData.filter((todo) => todo.completed);
+  const completedTodos = todos.filter((todo) => todo.completed);
+
+  const addNewTask = () => {
+    if (newTask) {
+      const newTodo = {
+        id: todos.length + 1,
+        text: newTask,
+        completed: false,
+        important: false,
+      };
+
+      setTodos((prev) => [...prev, newTodo]);
+      setNewTask("");
+    }
+  };
+
+  const handleSearch = (value) => {
+    if (value) {
+      const filteredTodos = todos.filter((todo) => todo.text.includes(value));
+      setTodos(filteredTodos);
+    } else {
+      setTodos(todosData);
+    }
+  };
 
   return (
     <div
@@ -30,6 +238,7 @@ export default function TaskList() {
               type="text"
               placeholder="Search todos..."
               className="outline-none"
+              onChange={(e) => handleSearch(e.target.value)}
             />
           </div>
           <div className="rounded-full p-1 bg-zinc-100 flex gap-1 dark:bg-zinc-900">
@@ -59,18 +268,29 @@ export default function TaskList() {
           <input type="checkbox" />
           <div className="px-4 border-2 border-[#D4D4D8] py-2 dark:border-zinc-600">
             <input
+              value={newTask}
+              onKeyDown={(e) => e.key === "Enter" && addNewTask()}
               type="text"
               placeholder="Add a new task..."
               className="outline-none w-[320px]"
+              onChange={(e) => setNewTask(e.target.value)}
             />
           </div>
         </div>
 
         {/* Important Tasks */}
-        <TaskCard todosData={importantTodos} title="Important" />
+        <TaskCard
+          todosData={importantTodos}
+          setTodos={setTodos}
+          title="Important"
+        />
 
         {/* Not Important */}
-        <TaskCard todosData={notImportantTodos} title="Everything Else" />
+        <TaskCard
+          todosData={notImportantTodos}
+          title="Everything Else"
+          setTodos={setTodos}
+        />
 
         {/* Completed */}
         <TaskCard todosData={completedTodos} title="Completed" />
